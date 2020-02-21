4c9ab9d93519957b7671e51b89c5ee4dc2008515
Julian Gonzalez Dos Reis <Julian@MacBook-Air-de-Julian.local>
2020-02-21 17:59:43 +0100

    Added python file to change the structure

---
+++ src/main/python/modifyInputFile.py
@@ -0,0 +1,13 @@
+"""
+Script designed to change the structure of the input file to match the
+k-means algorithm criteria
+"""
+import pandas as pd
+
+
+class ModifyInputFile(object):
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+
+print("hello world")
