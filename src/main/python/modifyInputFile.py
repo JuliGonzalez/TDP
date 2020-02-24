79080404d6bc4f4a216255aab9ba10bdded3b58f
Julian Gonzalez <julian.gonzalez@the-cocktail.com>
2020-02-24 12:10:12 +0100

    Added class methods to modify the input file

--- src/main/python/modifyInputFile.py
+++ src/main/python/modifyInputFile.py
@@ -9,5 +9,18 @@ class ModifyInputFile(object):
     def __init__(self, file_path):
         self.file_path = file_path
 
+    def get_headers(self):
+        pass
+
+    def add_new_columns(self, headers_to_include):
+        pass
+
+    def drop_old_columns(self, headers_to_remove):
+        pass
+
+    def export_updated_input_file(self, df):
+        df.to_csv("../../input/KDDTrainCleaned.csv", sep=',') # csv or txt, not sure yet
+
+
 
 print("hello world")
