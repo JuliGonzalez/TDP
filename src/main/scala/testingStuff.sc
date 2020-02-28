1c793a8273e43b441503cd7af36a3f890488e570
Julian Gonzalez <julian.gonzalez@the-cocktail.com>
2020-02-28 14:24:29 +0100

    Added Naive Bayes model and made it work correctly

--- src/main/scala/testingStuff.sc
+++ src/main/scala/testingStuff.sc
@@ -1,3 +1,25 @@
 "hello"
 3+4
 val test:String = "How is everyone doing tonight"
+
+import org.apache.spark.mllib.util.MLUtils
+import org.apache.spark.{SparkContext, SparkConf}
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.regression.LabeledPoint
+
+
+val conf = new SparkConf().setAppName("testingNaives").setMaster("local[*]")
+val sc = new SparkContext(conf)
+
+val data_path: String = "/home/juliangonzalezdosreis/ScalaProjects/TDP/input/sample_svm_data.txt"
+val data = sc.textFile(data_path)
+
+//val data = MLUtils.loadLibSVMFile(sc,data_path)
+val parsedData = data.map { line =>
+  val parts = line.split(',')
+  LabeledPoint(parts(0).toDouble, Vectors.dense(parts(1).split(' ').map(_.toDouble)))
+}
+
+print(data)
+print(parsedData)
+
--- src/main/scala/trainModel_naiveBayes.scala
+++ src/main/scala/trainModel_naiveBayes.scala
@@ -1,26 +1,29 @@
+import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.mllib.classification.{NaiveBayes, NaiveBayesModel}
 import org.apache.spark.mllib.util.MLUtils
-import org.apache.spark.{SparkContext, SparkConf}
 
+object NaiveBayesExample {
 
-object trainModel_naiveBayes extends App{
-  val conf = new SparkConf().setAppName("trainmodel_naiveBayes").setMaster("local[*]")
-  val sc = new SparkContext(conf)
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("NaiveBayesExample").setMaster("local[*]")
+    val sc = new SparkContext(conf)
+    // $example on$
+    // Load and parse the data file.
+    val data = MLUtils.loadLibSVMFile(sc, "input/sample_libsvm_data.txt")
 
-  val data_path: String = "input/sample_svm_data.txt"
+    // Split data into training (60%) and test (40%).
+    val Array(training, test) = data.randomSplit(Array(0.6, 0.4))
 
-  val data = MLUtils.loadLibSVMFile(sc, data_path)
-  val Array(training, test) = data.randomSplit(Array(0.6, 0.4))
+    val model = NaiveBayes.train(training, lambda = 1.0, modelType = "multinomial")
 
-  val model = NaiveBayes.train(training, lambda = 1.0, modelType = "multinomial")
+    val predictionAndLabel = test.map(p => (model.predict(p.features), p.label))
+    val accuracy = 1.0 * predictionAndLabel.filter(x => x._1 == x._2).count() / test.count()
 
-  val predictionAndLabel = test.map(p => (model.predict(p.features), p.label))
-  val accuracy = 1.0 * predictionAndLabel.filter(x => x._1 == x._2).count() / test.count()
-
-  model.save(sc, "models/NaiveBayes")
-
-  val sameModel = NaiveBayesModel.load(sc, "models/NaiveBayes")
-
-  sc.stop()
-
+    // Save and load model
+    model.save(sc, "models/myNaiveBayesModel")
+    val sameModel = NaiveBayesModel.load(sc, "models/myNaiveBayesModel")
+    // $example off$
+  }
 }
+
+// scalastyle:on println
