48ac2ce784f9c053a53b90408881e632f3af44e0
JulianGonzalez <juliangonzalezdosreis@gmail.com>
2020-03-30 11:31:32 +0200

    Modified consumer to serve localhost server

--- src/main/python/kafka-consumer.py
+++ src/main/python/kafka-consumer.py
@@ -3,7 +3,7 @@ from json import loads
 
 consumer = KafkaConsumer(
             'test',
-            auto_offset_reset='earliest',
+            auto_offset_reset='latest',
             enable_auto_commit=True,
             group_id='my-group-1',
             value_deserializer=lambda m: loads(m.decode('utf-8')),
@@ -11,5 +11,8 @@ consumer = KafkaConsumer(
             # bootstrap_servers=['172.17.0.1:32783','172.17.0.1:32782','172.17.0.1:32781']
          )
 
+print('entering here')
+print(consumer, type(consumer))
 for m in consumer:
+    print(type(m))
     print(m.value)
