5d79d2fcfc594345078cc78385c9f017d16bde58
JulianGonzalez <juliangonzalezdosreis@gmail.com>
2020-04-02 13:53:00 +0200

    Updated file structure inside source code folder

---
+++ src/main/scala/kafka/sink_connector/JdbcSinkConnector.scala
@@ -0,0 +1,13 @@
+package kafka.sink_connector
+
+import org.apache.kafka.common.config.{Config, ConfigDef}
+import org.apache.kafka.connect.connector.Task
+import org.apache.kafka.connect.sink.SinkConnector
+
+import java.util
+
+import io.confluent.conn
+
+class JdbcSinkConnector {
+
+}
