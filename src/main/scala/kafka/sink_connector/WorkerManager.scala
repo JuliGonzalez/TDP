2f7471adbb3c51baf91a5792036ba992f864b6ae
JulianGonzalez <juliangonzalezdosreis@gmail.com>
2020-04-06 10:53:12 +0200

    Added WorkerManager class to handle start and stop connection

--- src/main/scala/kafka/sink_connector/WorkerManager.scala
+++ src/main/scala/kafka/sink_connector/WorkerManager.scala
@@ -1,5 +1,21 @@
 package kafka.sink_connector
 
-class WorkerManager {
+import akka.actor.Actor
+import org.apache.kafka.connect.runtime.Connect
+import org.apache.kafka.trogdor.rest.StopWorkerRequest
 
+
+class WorkerManager(connect: Connect) extends Actor {
+
+  import WorkerManager._
+
+  override def receive: Receive = {
+    case StartWorker => connect.start()
+    case StopWorker => connect.stop()
+  }
+}
+
+object WorkerManager {
+  case object StartWorker
+  case object StopWorker
 }
