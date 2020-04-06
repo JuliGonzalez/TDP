dcc29ac676e884b75d518c73ed953832dfc72e72
JulianGonzalez <juliangonzalezdosreis@gmail.com>
2020-04-06 12:01:12 +0200

    Muted whole class til testing is done

--- src/main/scala/kafka/sink_connector/SinkConnector.scala
+++ src/main/scala/kafka/sink_connector/SinkConnector.scala
@@ -4,9 +4,11 @@ import io.confluent.connect.jdbc.JdbcSinkConnector
 import org.apache.kafka.connect
 
 object SinkConnector extends App{
+  /*
   val connect: JdbcSinkConnector = new JdbcSinkConnector()
   println(connect.config())
   println(connect.version())
   connect.taskConfigs(1)
   connect.start()
+   */
 }
