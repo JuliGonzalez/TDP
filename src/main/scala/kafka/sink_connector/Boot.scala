9fb5fbbd18e63a4725a919f8e362a2cf6df878a8
JulianGonzalez <juliangonzalezdosreis@gmail.com>
2020-04-06 10:59:28 +0200

    Updated Boot class to execute the kafka-connector processs

--- src/main/scala/kafka/sink_connector/Boot.scala
+++ src/main/scala/kafka/sink_connector/Boot.scala
@@ -1,5 +1,22 @@
 package kafka.sink_connector
 
-class Boot {
+import akka.actor.ActorSystem
+import kafka.sink_connector.WorkerManager.StartWorker
 
+import scala.concurrent.{ExecutionContext, ExecutionContextExecutor}
+import scala.language.postfixOps
+
+class Boot extends App{
+  def run()(implicit system: ActorSystem, ec: ExecutionContext): Unit = {
+    val assembly = new Assembly
+    import assembly._
+
+    workerManager ! StartWorker
+  }
+
+  override def main(args: Array[String]): Unit = {
+    implicit val actorSystem: ActorSystem = ActorSystem("kafka-connect")
+    implicit val executor: ExecutionContextExecutor = actorSystem.dispatcher
+    run()
+  }
 }
