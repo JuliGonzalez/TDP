39ae1692082807f819fabd4539bd6544901582dd
JulianGonzalez <juliangonzalezdosreis@gmail.com>
2020-04-06 12:01:46 +0200

    Updated WorkerManager

--- src/main/scala/kafka/sink_connector/WorkerManager.scala
+++ src/main/scala/kafka/sink_connector/WorkerManager.scala
@@ -2,8 +2,6 @@ package kafka.sink_connector
 
 import akka.actor.Actor
 import org.apache.kafka.connect.runtime.Connect
-import org.apache.kafka.trogdor.rest.StopWorkerRequest
-
 
 class WorkerManager(connect: Connect) extends Actor {
 
