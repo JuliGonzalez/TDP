9fb391b165dd7a72688b0612ba7693a977953bd1
JulianGonzalez <juliangonzalezdosreis@gmail.com>
2020-04-15 12:05:09 +0200

    Added config classes for jdbc sink connector

---
+++ src/main/scala/kafka/config/Personal/PersonalParameters.scala
@@ -0,0 +1,32 @@
+package kafka.config.Personal
+
+import java.util
+
+import com.sap.kafka.client.hana.HANAConfigMissingException
+import kafka.config.BaseParameters
+
+import scala.collection.JavaConversions._
+
+object PersonalParameters extends BaseParameters {
+
+  override def getConfig(props: util.Map[String, String]): HANAConfig = {
+    super.getConfig(props)
+
+    if (props.get("connection.url") == null) {
+      throw new HANAConfigMissingException("Mandatory parameter missing: " +
+        " HANA DB Jdbc url must be specified in 'connection.url' parameter")
+    }
+
+    if (props.get("connection.user") == null) {
+      throw new HANAConfigMissingException("Mandatory parameter missing: " +
+        " HANA DB user must be specified in 'connection.user' parameter")
+    }
+
+    if (props.get("connection.password") == null) {
+      throw new HANAConfigMissingException("Mandatory parameter missing: " +
+        " HANA DB password must be specified in 'connection.password' parameter")
+    }
+
+    HANAConfig(props.toMap)
+  }
+}
